7e5cfdf6e5fa655f22bd03a8332d67116f8c0d01
Alan Meira <alanmeira@gmail.com>
2023-09-09 00:40:46 -0300

    updates todoMVC

--- packages/todoMVC/src/app/index.view.js
+++ packages/todoMVC/src/app/index.view.js
@@ -1,5 +1,5 @@
 export default {
-  tag: "start-bootstrapp",
+  tag: "app-index",
   render: function ({ html }) {
     return html`<todo-app class="p-4"></todo-app>`;
   },
--- packages/todoMVC/src/app/views/todo-item.view.js
+++ packages/todoMVC/src/app/views/todo-item.view.js
@@ -1,3 +1,8 @@
+import "@vaadin/vaadin-text-field";
+import "@vaadin/vaadin-button";
+import "@vaadin/vaadin-checkbox";
+import "@vaadin/vaadin-list-box";
+
 export default {
   tag: "todo-item",
   props: {
@@ -16,21 +21,19 @@ export default {
           ? "line-through text-gray-400"
           : ""}"
       >
-        <div class="view flex items-center space-x-2">
-          <input
-            class="toggle rounded focus:ring-2 focus:ring-blue-400"
-            type="checkbox"
-            .checked=${todo.completed}
-            @click=${toggle}
-          />
-          <label class="flex-grow">${todo.title}</label>
-          <button
-            class="destroy text-red-500 hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 rounded"
-            @click=${remove}
-          >
-            X
-          </button>
-        </div>
+        <vaadin-checkbox
+          class="toggle rounded focus:ring-2 focus:ring-blue-400"
+          .checked=${todo.completed}
+          @change=${toggle}
+        >
+          ${todo.title}
+        </vaadin-checkbox>
+        <vaadin-button
+          class="destroy text-red-500 hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 rounded"
+          @click=${remove}
+        >
+          X
+        </vaadin-button>
       </li>
     `;
   },
