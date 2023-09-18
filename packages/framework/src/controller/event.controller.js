a58cd49c79132455a53e57ffda537ae47f6a4b64
Alan Meira <alanmeira@gmail.com>
2023-09-18 16:28:31 -0300

    updates

--- packages/framework/src/controller/event.controller.js
+++ packages/framework/src/controller/event.controller.js
@@ -1,70 +1,29 @@
-const eventHandlers = {};
-/**
- * @callback SubscriberCallback
- * @param {*} value
- */
+export const eventHandlers = {};
+export const queue = [];
 
-/**
- * @callback EventHandler
- * @param {*} data
- */
 
-/**
- * Class representing event handlers.
- */
-export class EventHandlers {
-  constructor() {
-    /** @private @type {Object.<string, EventHandler>} */
-    this.handlers = {};
-  }
+export const register = (eventType, handler) => {
+  eventHandlers[eventType] = handler;
+};
   
-  /**
-     * Register a handler for an event type.
-     * @param {string} eventType - Type of the event.
-     * @param {EventHandler} handler - The handler for the event.
-     */
-  register(eventType, handler) {
-    this.handlers[eventType] = handler;
+export const handle =(event) => {
+  if (eventHandlers[event.type]) {
+    eventHandlers[event.type](event);
   }
+};
+
+export const dispatchEvent = (key, params = {}) => {
+  const eventObj = {
+    type: key, 
+    ...params
+  };
+  queue.push(eventObj);
+  process();
+};
   
-  /**
-     * Handle an event.
-     * @param {*} event - The event to handle.
-     */
-  handle(event) {
-    if (this.handlers[event.type]) {
-      this.handlers[event.type](event);
-    }
+export const process = () => {
+  while (queue.length) {
+    const event = queue.shift();
+    eventHandlers.handle(event); 
   }
-}
-  
-/**
-   * Class representing an event queue.
-   */
-export class EventQueue {
-  constructor() {
-    /** @private @type {Array<*>} */
-    this.queue = [];
-  }
-  
-  /**
-     * Enqueue an event.
-     * @param {*} event - The event to enqueue.
-     */
-  enqueue(event) {
-    this.queue.push(event);
-    this.process();
-  }
-  
-  /**
-     * Process the event queue.
-     * @private
-     */
-  process() {
-    while (this.queue.length) {
-      const event = this.queue.shift();
-      eventHandlers.handle(event); // Assuming `eventHandlers` is a global reference. Modify if needed.
-    }
-  }
-}
-  
+};
--- packages/framework/src/controller/reactive-controller.js
+++ packages/framework/src/controller/reactive-controller.js
@@ -1,109 +1,28 @@
-import { EventHandlers, EventQueue } from "./event.controller.js";
-
-export const eventHandlers = new EventHandlers();
-export const eventQueue = new EventQueue();
-
-export class ReactiveController {
-
-
-  static subscribers = [];
-    
-  constructor(appState) {
-    /** @private @type {Object} */
-    this.subscribers = {};
-    /** @private @type {Object} */
-    this.eventHandlers = {};
-    this.appState = appState;
-    this.subscriptionCallbacks = {};
-  }
-
-  /**
-     * Subscribe to state changes based on a key.
-     * @param {string} key 
-     * @param {function} callback 
-     */
-  async subscribe(key, callback) {
-    if (!this.subscribers[key]) {
-      this.subscribers[key] = [];
-    }
-    this.subscribers[key].push(callback);    
-    callback(await this.appState.get(key)); // Get initial value if exists
-  }
-
-  /**
-     * Unsubscribe a callback from a key.
-     * @param {string} key 
-     * @param {function} callback 
-     */
-  unsubscribe(key, callback) {
-    if (this.subscribers[key]) {
-      this.subscribers[key] = this.subscribers[key].filter(
-        (sub) => sub !== callback
-      );
-      if (this.subscribers[key].length === 0) {
-        delete this.subscribers[key];
-      }
-    }
-  }
-
-  /**
-     * Notify all subscribers of a particular key about its new value.
-     * @param {string} key 
-     * @param {*} newValue 
-     */
-  notify(key, newValue) {
-    if (this.subscribers[key]) {
-      for (const callback of this.subscribers[key]) {
-        callback(newValue);
-      }
-    }
-  }
-    
-  static notifyAll() {
-    for (const subscriber of ReactiveController.subscribers) {
-      console.log({subscriber});
-      if (subscriber && typeof subscriber.requestUpdate === "function") {
-        subscriber.requestUpdate();
-      }
-    }
-  }
-
-  /**
-     * Register an event handler.
-     * @param {string} event 
-     * @param {function} handler 
-     */
-    
-  /**
-   * Example of extending with an event handler.
-  initHandlers() {
-    this.on('TOGGLE_DARK_MODE', () => {
-      this.setState('darkMode', !this.state.get('darkMode'));
-    });
-  }
-   */
-  on(event, handler) {
-    if (!this.eventHandlers[event]) {
-      this.eventHandlers[event] = [];
-    }
-    this.eventHandlers[event].push(handler);
-  }
-}
+import { register } from "./event.controller.js";
 
 export function defineController(controller, convention) {
-  return class ActionController extends ReactiveController {
+  const { events } = controller;
+  return class ReactiveController {
+    static subscribers = [];
     constructor(host, appState) {
-      super(host, appState);
+      this.appState = appState;
+      this.subscriptionCallbacks = {};
       this.appState = appState;
       this.host = host;
       this.host.addController(this);
       if(host.reactive) {
-        this.constructor.subscribers.push(host);
+        ReactiveController.subscribers.push(host);
         const get = async () => {
           return await this.appState.list(this.modelName);
         };
         // TODO: create set() function to update on the subscribe
         Object.defineProperty(this.host, "list", { get });
+      }
+
+      if (events) {
+        Object.keys(events).forEach((eventKey) => {
+          register(eventKey, events[eventKey]);
+        });
       }
       
       this.modelName = convention?.modelName;
@@ -115,31 +34,60 @@ export function defineController(controller, convention) {
       });
     }
 
+    static notifyAll() {
+      for (const subscriber of ReactiveController.subscribers) {
+        if (subscriber && typeof subscriber.requestUpdate === "function") {
+          subscriber.requestUpdate();
+        }
+      }
+    }
+
     add = async (record) => {
       await this.appState.add(record);
-      ActionController.notifyAll();    
+      ReactiveController.notifyAll();    
     };
 
 
     addMany = async (records) => {
       await this.appState.addMany(records);
-      ActionController.notifyAll();
+      ReactiveController.notifyAll();
     };
 
     edit = async (id, updates) => {
       await this.appState.edit(id, updates);
-      ActionController.notifyAll();    
+      ReactiveController.notifyAll();    
     };
 
 
     editMany = async (updates) => {
       await this.appState.editMany([updates]);
-      ActionController.notifyAll();    
+      ReactiveController.notifyAll();    
     };
 
     remove = async (id) => {
       await this.appState.remove(id);
-      ActionController.notifyAll();    
+      ReactiveController.notifyAll();    
     };
+
+
+    async subscribe(key, callback) {
+      if (!ReactiveController.subscribers[key]) {
+        ReactiveController.subscribers[key] = [];
+      }
+      ReactiveController.subscribers[key].push(callback);    
+      callback(await this.appState.get(key)); 
+    }
+
+    unsubscribe(key, callback) {
+      if (ReactiveController.subscribers[key]) {
+        ReactiveController.subscribers[key] = ReactiveController.subscribers[key].filter(
+          (sub) => sub !== callback
+        );
+        if (ReactiveController.subscribers[key].length === 0) {
+          delete ReactiveController.subscribers[key];
+        }
+      }
+    }
   };
+
 };
