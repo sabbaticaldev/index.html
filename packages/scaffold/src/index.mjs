55de608b84fc44664b786bee5b0e691dd160861e
Alan Meira <alanmeira@gmail.com>
2023-10-13 12:55:01 -0300

    updates

--- packages/scaffold/src/index.mjs
+++ packages/scaffold/src/index.mjs
@@ -1,14 +1,14 @@
 import { definePackage } from "./reactive-view.mjs";
 
-import uiKit from "./packages/ui.package.mjs";
-import uxKit from "./packages/ux.package.mjs";
-import appKit from "./packages/app.package.mjs";
-import formKit from "./packages/form.package.mjs";
-import typographyKit from "./packages/typography.package.mjs";
-import docsKit from "./packages/docs.package.mjs";
-import navigationKit from "./packages/navigation.package.mjs";
-import layoutKit from "./packages/layout.package.mjs";
-import contentKit from "./packages/content.package.mjs";
+import uiKit from "uix/packages/ui.package.mjs";
+import uxKit from "uix/packages/ux.package.mjs";
+import appKit from "uix/packages/app.package.mjs";
+import formKit from "uix/packages/form.package.mjs";
+import typographyKit from "uix/packages/typography.package.mjs";
+import docsKit from "uix/packages/docs.package.mjs";
+import navigationKit from "uix/packages/navigation.package.mjs";
+import layoutKit from "uix/packages/layout.package.mjs";
+import contentKit from "uix/packages/content.package.mjs";
 
 const definePackages = (packages, options) => {
   return packages.reduce(
