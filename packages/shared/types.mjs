7c5e3d268d27b7e3eb54dc77e1f5ed7bf128ac8b
Alan Meira <alanmeira@gmail.com>
2023-10-15 19:17:59 -0300

    updates

--- packages/shared/types.mjs
+++ packages/shared/types.mjs
@@ -2,108 +2,94 @@ export const T = {
   boolean: (options = {}) => ({
     type: "boolean",
     defaultValue: options.defaultValue ?? false,
-    ...options
+    ...options,
   }),
 
   string: (options = {}) => ({
     type: "string",
     defaultValue: options.defaultValue || "",
     enum: options.enum || [],
-    ...options
+    ...options,
   }),
 
   array: (options = {}) => ({
     type: "array",
     defaultValue: options.defaultValue || [],
     enum: options.enum || [],
-    ...options
+    ...options,
   }),
 
   number: (options = {}) => ({
     type: "number",
     defaultValue: options.defaultValue || undefined,
-    ...options
+    ...options,
   }),
 
   date: (options = {}) => ({
     type: "date",
     defaultValue: options.defaultValue || undefined,
-    ...options
+    ...options,
   }),
 
   function: (options = {}) => ({
     type: "function",
     defaultValue: options.defaultValue || undefined,
-    ...options
+    ...options,
   }),
 
   object: (options = {}) => ({
     type: "object",
     defaultValue: options.defaultValue || undefined,
-    ...options
+    ...options,
   }),
   one: (relationship, targetForeignKey, options = {}) => ({
     type: "one",
     relationship,
     targetForeignKey: targetForeignKey,
-    ...options
+    ...options,
   }),
   many: (relationship, targetForeignKey, options = {}) => ({
     type: "many",
     relationship,
     targetForeignKey: targetForeignKey,
-    ...options
-  })
-};
-
-export const F = {
+    ...options,
+  }),
   text: (options = {}) => ({
     formType: "text",
-    type: T.string(options)
+    type: T.string(options),
   }),
-
-  number: (options = {}) => ({
-    formType: "number",
-    type: T.number(options)
-  }),
-
-  date: (options = {}) => ({
-    formType: "date",
-    type: T.date(options)
-  }),
-
   datetime: (options = {}) => ({
     formType: "datetime",
-    type: T.string(options)
+    type: T.string(options),
   }),
 
   time: (options = {}) => ({
     formType: "time",
-    type: T.string(options)
+    type: T.string(options),
   }),
 
   checkbox: (options = {}) => ({
     formType: "checkbox",
-    type: T.boolean(options)
+    type: T.boolean(options),
   }),
 
   radio: (options = {}) => ({
     formType: "radio",
-    type: T.boolean(options)
+    type: T.boolean(options),
   }),
 
   toggle: (options = {}) => ({
     formType: "toggle",
-    type: T.boolean(options)
+    type: T.boolean(options),
   }),
 
   richText: (options = {}) => ({
     formType: "richText",
-    type: T.string(options)
+    type: T.string(options),
   }),
 
   custom: (customFormType, options) => ({
     customFormType,
-    type: T[customFormType](options)
-  })
+    type: T[customFormType](options),
+  }),
 };
