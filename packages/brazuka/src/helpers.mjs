8e3b2e2f1778c63590910169b2a98c4ab66744f0
Alan Meira <alanmeira@gmail.com>
2023-10-22 11:09:41 -0300

    updates

---
+++ packages/brazuka/src/helpers.mjs
@@ -0,0 +1,9 @@
+import T from "./helpers/types.mjs";
+import { get, post, patch, remove } from "./helpers/rest.mjs";
+import { droparea, draggable } from "./helpers/droparea.mjs";
+import datetime from "./helpers/datetime.mjs";
+import url from "./helpers/url.mjs";
+
+export { T, droparea, draggable, datetime, get, post, patch, remove, url };
+
+export default T;
--- packages/shared/index.mjs
+++
@@ -1,9 +0,0 @@
-import T from "./src/types.mjs";
-import { get, post, patch, remove } from "./src/rest.mjs";
-import { droparea, draggable } from "./src/droparea.mjs";
-import datetime from "./src/datetime.mjs";
-import url from "./src/url.mjs";
-
-export { T, droparea, draggable, datetime, get, post, patch, remove, url };
-
-export default T;
