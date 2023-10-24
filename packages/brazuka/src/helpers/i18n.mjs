077254b69c80057e094972a89dd51331d3913fec
Alan Meira <alanmeira@gmail.com>
2023-10-24 11:13:19 -0300

    moved folders

--- packages/brazuka/src/helpers/i18n.mjs
+++
@@ -1,6 +0,0 @@
-// TODO: make i18n integrate with localStorage to store the current language and alternatives
-// Then make every translation available to every component
-
-const i18n = (key) => key;
-
-export default i18n;
--- packages/brazuka/src/uix/tests.package.mjs
+++
@@ -1,7 +0,0 @@
-import {} from "../style-props.mjs";
-
-// TODO: create a package that supports creating / running tests
-export default {
-  i18n: {},
-  views: {},
-};
