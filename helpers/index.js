62e5835b59dc784a40b4d126a3060e4ac43b246d
Alan Meira <alanmeira@gmail.com>
2023-11-10 19:24:55 -0300

    chore: start models spaces

--- helpers/index.js
+++ helpers/index.js
@@ -4,7 +4,17 @@ import { droparea, draggable } from "./droparea.js";
 import datetime from "./datetime.js";
 import url from "./url.js";
 import i18n from "./i18n.js";
+
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
 export {
+  debounce,
   T,
   i18n,
   get,
@@ -14,5 +24,5 @@ export {
   droparea,
   draggable,
   datetime,
-  url
+  url,
 };
