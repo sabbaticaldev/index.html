19c8c83e58e7f0f91dffc63e88a194eb6f955d22
Alan Meira <alanmeira@gmail.com>
2023-11-17 12:35:40 -0300

    Updates

--- frontend/uix/crud.package.js
+++ frontend/uix/crud.package.js
@@ -4,13 +4,13 @@ import {
   ref,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@3.0.0/all/lit-all.min.js";
 
+import * as CSV from "../helpers/csv.js";
 import { post } from "../helpers/rest.js";
 import { T } from "../helpers/types.js";
-
 export default {
   i18n: {},
   views: {
-    "uix-create-new-modal": {
+    "uix-crud-new-modal": {
       props: {
         fields: T.array(),
         rows: T.array(),
@@ -51,6 +51,85 @@ export default {
   ]}
           ></uix-form>
         </uix-modal>`;
+      },
+    },
+
+    "app-import-csv-button": {
+      props: {
+        fields: T.array(),
+        rows: T.array(),
+        setRows: T.function(),
+        model: T.string(),
+        CSVRows: T.array(),
+        CSVFields: T.array(),
+      },
+      handleFileChange: async function (e) {
+        try {
+          const file = e.target.files[0];
+          const csvContent = await File.readFile(file);
+          let data = CSV.parseCSV(csvContent);
+          this.setCSVRows(data);
+          this.setCSVFields(CSV.getFields(data));
+          this.q("uix-modal").show();
+        } catch (error) {
+          console.error("Error processing file:", error);
+        }
+      },
+      render: function () {
+        const { setRows, CSVRows, fields = [], CSVFields, model } = this;
+        const form = this.q("uix-form");
+        return html` <uix-button
+            @click=${() => this.q("#ImportCSVFileInput").click()}
+            size="xs"
+            variant="secondary"
+          >
+            Import CSV
+          </uix-button>
+          <uix-modal>
+            <input
+              type="file"
+              id="ImportCSVFileInput"
+              accept=".csv"
+              style="display: none;"
+              @change=${this.handleFileChange}
+            />
+            <uix-list vertical>
+              <uix-text size="lg" weight="bold">
+                Select the matching CSV fields:
+              </uix-text>
+              ${html`<uix-form
+                title="New"
+                color="base"
+                size="md"
+                name="uixCRUDForm"
+                .fields=${fields.map((field) => ({
+    ...field,
+    name: field,
+    type: "select",
+    options: ["choose a field for " + field, ...CSVFields],
+    placeholder: field,
+  }))}
+                .actions=${[
+    {
+      label: "Import " + this.model,
+      type: "submit",
+      click: () => {
+        const fieldMapping = form.formData();
+        const rows = CSV.transformCsvData(CSVRows, fieldMapping);
+        if (form.validate()) {
+          post(model, rows).then((newPosts) => {
+            setRows([...rows, newPosts]);
+          });
+          form.reset();
+          this.q("uix-modal").hide();
+        }
+      },
+    },
+  ]}
+              >
+              </uix-form>`}
+            </uix-list>
+          </uix-modal>`;
       },
     },
   },
