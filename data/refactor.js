35e5612c95630eda6d81246dc459b38b9472c12c
Alan Meira <alanmeira@gmail.com>
2024-05-26 20:31:14 -0300

    Refactored accordion, card and tabs sections to use properties instead of slots for subcomponents

--- data/refactor.js
+++ data/refactor.js
@@ -1,26 +1,9 @@
 export default {
-  contextSrc: ["libs/frontend", "libs/helpers"],
+  contextSrc: ["libs/frontend", "app/apps/design"],
   refactoringFiles:
-    "helpers/types.js and other files that are affected by this change",
+    "all files related to the change of flattening subcomponents in app/apps/design",
   taskPrompt: `We are creating a UI library based on Lit framework. We created our own format to create those components as you can se in the apps/design files and uix/ files.
-we just refactored our code to have a type of Array that is bit more complex but our system doesnt support it yet:
-+         export default {
-  props: {
-    allowMultiple: T.boolean(),
-+    items: T.array({
-+      defaultValue: [],
-+      type: {
-+        label: T.string(),
-+        content: T.string(),
-+        open: T.boolean(),
-+      },
-+    }),
-  },
-  render() {
-    return html
-    ........
-  }
-   so we need to refactor our code to support this. maybe we could also expand our type system to be more complete and support better typing, like the type could be a T.object({})  and we have a way to describe the T.object ? what do you suggest? (show me only the code)
-  `,
+  Now I want your help reviewing all the components and the structure we have and help me create the building blocks components that are missing for a complete solution were we can create all blocks for apps and landing pages. 
+  We just refactored all components that had subcomponents and flattened them, now we need to refactor the docs to reflect the new format for tabs, card and accordion. So for example card would have a header, body and footer properties that could be an object/element or a string.  Same for accordion, tabs and other components that use unnecessary slots with subcomponents.`,
   responseFormat: "xml",
 };
--- libs/frontend/uix/layout/accordion.js
+++ libs/frontend/uix/layout/accordion.js
@@ -12,6 +12,19 @@ export default {
       },
     }),
   },
+
+  theme: {
+    "uix-accordion": "",
+    "uix-accordion-item": {
+      _base: "border-b border-gray-200",
+      open: { true: "bg-gray-100" },
+    },
+    "uix-accordion-item__header": "p-4 cursor-pointer",
+    "uix-accordion-item__content": {
+      _base: "p-4",
+      open: { true: "block", false: "hidden" },
+    },
+  },
   render() {
     return html`
       <div class=${this.theme("uix-accordion")}>
