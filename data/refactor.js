99c3d49e9fa21c413823d0be3daaa095b873c060
Alan Meira <alanmeira@gmail.com>
2024-05-27 15:11:58 -0300

    Refactored app/apps/design/app.js to use package.js files for each section, reducing the number of imports. Updated app.js logic to handle the new format of receiving sections/components.

--- data/refactor.js
+++ data/refactor.js
@@ -1,8 +1,9 @@
 export default {
   contextSrc: ["libs/frontend", "app/apps/design"],
-  refactoringFiles: "the refactored app/apps/design/app.js",
+  refactoringFiles:
+    "the refactored app/apps/design/app.js, the added package.js files for each section",
   taskPrompt: `We are creating a UI library based on Lit framework. We created our own format to create those components as you can se in the apps/design files and uix/ files.
-  Lets refactor the sections to reflect the current uix/ and sections/ described components
+  let's refactor design/app.js to reduce the amount of imports. Let's create a package.js for each section and include there the imports and then in design/app.js import the sections. Update app.js logic to accept this new format of receiving the sections/components
   `,
   responseFormat: "xml",
 };
