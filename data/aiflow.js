058abe0c03ced58a43b76cae26b05b9ac3c0b057
Alan Meira <alanmeira@gmail.com>
2024-06-10 18:24:09 -0300

    refactor: create markdown documentation files for each page component

--- data/aiflow.js
+++ data/aiflow.js
@@ -6,7 +6,6 @@ export default {
   We are creating the documentation for the uix components in our frontend library. In docs/ we have an example of how should we organize our docs markdown files
 
   Lets create one task for each component. Describe in the task how the markdown files should be and add the input.md as an example. Make the description of each task have enough information for it to generate the markdown later. 
-
-  lets do it for form and page components
-`,
+create one task for each macro sets of components. Add in the description a list of all components to have their markdown created or ignore it if it already exists
+  `,
 };
