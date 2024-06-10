a6e5628a95cbfd2e70f80b016e0dda25a93904ca
Alan Meira <alanmeira@gmail.com>
2024-06-10 18:44:36 -0300

    refactor: created new markdown files for form components

--- data/refactor-diff-frontend.js
+++ data/refactor-diff-frontend.js
@@ -6,14 +6,9 @@ export default {
    We are creating a UI library based on Lit framework. We created our own format to create those components as you can see in libs/frontend/uix.    
   Now we are creating the the design documentation in the app/design dir. We created the first form input.md, lets create the other forms components documentation
 
-  save them in app/design/form/
+  We already created the markdown files. Now we need to create a package.js in each dir with an array with the elements as the default export. ["input", "range", ...]
 
-  dont repeat components that already have markdown file
-
-  use app/design/form/input.md as reference. Make all .md as complete as input.md
-
-  it should have the same elements. And twice the code example one as code and one as html that will be rendered
-  
+  it will be used later to create the menu
     `,
 
   responseFormat: "diff",
